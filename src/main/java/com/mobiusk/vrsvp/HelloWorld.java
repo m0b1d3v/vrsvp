30ef52b549ddb8b22083755653844e6e668bc1d2
Mobius K <mail@mobiusk.com>
2023-05-07 21:59:51 -0400

    Adding Javalin application startup with root route

--- src/main/java/com/mobiusk/vrsvp/HelloWorld.java
+++ src/main/java/com/mobiusk/vrsvp/HelloWorld.java
@@ -1,13 +1,18 @@
 package com.mobiusk.vrsvp;
 
+import io.javalin.Javalin;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
 		var hello = new HelloWorld();
 		var greeting = hello.greeting();
+		hello.createApp(greeting).start();
+	}
 
-		System.out.println(greeting);
+	public Javalin createApp(String greeting) {
+		return Javalin.create()
+			.get("/", context -> context.result(greeting));
 	}
 
 	public String greeting() {
