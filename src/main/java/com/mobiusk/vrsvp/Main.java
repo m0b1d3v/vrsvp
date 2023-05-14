fecdeac80493b09d45ca37c32c23d25d4bfa92a2
Mobius K <mail@mobiusk.com>
2023-05-13 23:34:46 -0400

    Removing Javalin and all HTTP server code
    I'm fairly sure I can do this without a web server at all.
    Using message text as application state removes need for a database too.

--- src/main/java/com/mobiusk/vrsvp/Main.java
+++ src/main/java/com/mobiusk/vrsvp/Main.java
@@ -3,22 +3,10 @@ package com.mobiusk.vrsvp;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		startDiscordBot();
-		startServer();
-	}
-
-	private static void startDiscordBot() throws InterruptedException {
 		var discordBot = new DiscordBot();
 		discordBot.create();
 		discordBot.configure();
 		discordBot.start();
-	}
-
-	private static void startServer() {
-		var httpServer = new HttpServer();
-		httpServer.create();
-		httpServer.configure("Hello, world");
-		httpServer.start();
 	}
 
 }
--- src/test/java/com/mobiusk/vrsvp/TestBase.java
+++ src/test/java/com/mobiusk/vrsvp/TestBase.java
@@ -1,10 +1,6 @@
 package com.mobiusk.vrsvp;
 
-import io.javalin.Javalin;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
@@ -12,16 +8,5 @@ import org.mockito.quality.Strictness;
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public abstract class TestBase {
-
-	@InjectMocks
-	protected HttpServer httpServer;
-
-	@Spy
-	protected Javalin javalin;
-
-	@BeforeEach
-	void beforeEach() {
-		httpServer.configure("Testing");
-	}
-
+	// Empty
 }
