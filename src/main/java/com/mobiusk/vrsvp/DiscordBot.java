183bddbb49267164b58d0e9fd1e3f4a92813223e
Mobius K <mail@mobiusk.com>
2023-05-14 00:36:59 -0400

    Changing JDABuilder to create API with no caching or message intents

--- src/main/java/com/mobiusk/vrsvp/DiscordBot.java
+++ src/main/java/com/mobiusk/vrsvp/DiscordBot.java
@@ -2,15 +2,20 @@ package com.mobiusk.vrsvp;
 
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.EnumSet;
 
 @RequiredArgsConstructor
 public class DiscordBot {
 
 	private final JDABuilder jdaBuilder;
 
+	/**
+	 * Create a Java Discord API gateway with no intents desired and using low memory by not caching any information.
+	 */
 	public static JDABuilder create(String discordBotToken) {
-		return JDABuilder.createDefault(discordBotToken);
+		return JDABuilder.createLight(discordBotToken, EnumSet.noneOf(GatewayIntent.class));
 	}
 
 	public void configure() {
