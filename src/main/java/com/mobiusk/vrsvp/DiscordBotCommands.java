b8685ef7280a7946bfd0528267302282d457ba52
Mobius K <mail@mobiusk.com>
2023-05-14 02:46:53 -0400

    Adding autocomplete to required input options on slash command

--- src/main/java/com/mobiusk/vrsvp/DiscordBotCommands.java
+++ src/main/java/com/mobiusk/vrsvp/DiscordBotCommands.java
@@ -6,10 +6,19 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
+import java.util.List;
+import java.util.Map;
+
 @UtilityClass
 public class DiscordBotCommands {
 
 	public static final String SLASH = "vrsvp";
+
+	public static final Map<String, List<Long>> INPUT_AUTOCOMPLETE_OPTIONS = Map.of(
+		DiscordBotCommandInputsEnum.BLOCKS.getInput(), List.of(1L, 2L, 3L, 4L),
+		DiscordBotCommandInputsEnum.SLOTS.getInput(), List.of(1L, 2L, 3L, 4L),
+		DiscordBotCommandInputsEnum.DURATION.getInput(), List.of(60L, 30L, 20L, 15L)
+	);
 
 	/**
 	 * Builds the only slash command this bot concerns itself with and supplies the required input options
@@ -19,16 +28,16 @@ public class DiscordBotCommands {
 		var data = Commands.slash(SLASH, "Create virtual RSVP")
 			.setDefaultPermissions(DefaultMemberPermissions.DISABLED);
 
-		addOption(data, DiscordBotCommandInputsEnum.START, "Timestamp from https://hammertime.cyou/ like '1684043839'");
-		addOption(data, DiscordBotCommandInputsEnum.BLOCKS, "One each for separate set of slots, like for each DJ at a rave");
-		addOption(data, DiscordBotCommandInputsEnum.SLOTS, "How many slots to have for each block");
-		addOption(data, DiscordBotCommandInputsEnum.DURATION, "How long each slot will last in minutes");
+		addOption(data, DiscordBotCommandInputsEnum.START, "Timestamp from https://hammertime.cyou/ like '1684043839'", false);
+		addOption(data, DiscordBotCommandInputsEnum.BLOCKS, "One each for separate set of slots, like for each DJ at a rave", true);
+		addOption(data, DiscordBotCommandInputsEnum.SLOTS, "How many slots to have for each block", true);
+		addOption(data, DiscordBotCommandInputsEnum.DURATION, "How long each slot will last in minutes", true);
 
 		return data;
 	}
 
-	private static void addOption(SlashCommandData data, DiscordBotCommandInputsEnum input, String description) {
-		data.addOption(OptionType.INTEGER, input.getInput(), description, true, false);
+	private static void addOption(SlashCommandData data, DiscordBotCommandInputsEnum input, String description, boolean autoComplete) {
+		data.addOption(OptionType.INTEGER, input.getInput(), description, true, autoComplete);
 	}
 
 }
--- src/main/java/com/mobiusk/vrsvp/DiscordBotEventListener.java
+++ src/main/java/com/mobiusk/vrsvp/DiscordBotEventListener.java
@@ -1,12 +1,32 @@
 package com.mobiusk.vrsvp;
 
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
 
 public class DiscordBotEventListener extends ListenerAdapter {
 
+	/**
+	 * Supplies auto-complete options for our named inputs during RSVP creation.
+	 */
+	@Override
+	public void onCommandAutoCompleteInteraction(@Nonnull CommandAutoCompleteInteractionEvent event) {
+
+		var inputName = event.getFocusedOption().getName();
+
+		var autoCompleteOptions = DiscordBotCommands.INPUT_AUTOCOMPLETE_OPTIONS.getOrDefault(inputName, Collections.emptyList());
+
+		if ( ! autoCompleteOptions.isEmpty()) {
+			event.replyChoiceLongs(autoCompleteOptions).queue();
+		}
+	}
+
+	/**
+	 * When slash command is submitted to create an RSVP, the magic will happen here.
+	 */
 	@Override
 	public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
 		// Empty
