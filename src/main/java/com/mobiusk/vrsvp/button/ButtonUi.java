6a5088090453777a8f2ef3b7e25395eb9fd31819
Mobius K <mail@mobiusk.com>
2023-06-08 19:33:30 -0400

    Changing RSVP form edit by signup toggle from embed to plain text

--- src/main/java/com/mobiusk/vrsvp/button/ButtonUi.java
+++ src/main/java/com/mobiusk/vrsvp/button/ButtonUi.java
@@ -1,9 +1,12 @@
 package com.mobiusk.vrsvp.button;
 
+import com.mobiusk.vrsvp.util.Parser;
 import lombok.experimental.UtilityClass;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
+import javax.annotation.Nonnull;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -51,6 +54,53 @@ public class ButtonUi {
 		}
 
 		return ActionRow.partitionOf(buttons);
+	}
+
+	/**
+	 * Toggle (add or remove) a user's mention to the specified slot for the given message.
+	 */
+	public static String editRsvpFromSignupButtonPress(
+		@Nonnull Message message,
+		@Nonnull String userMention,
+		int slotIndexDestination
+	) {
+
+		var slotIndex = 0;
+
+		var description = Parser.readMessageDescription(message);
+
+		var descriptionLines = new LinkedList<>(description.lines().toList());
+
+		for (var lineIndex = 0; lineIndex < descriptionLines.size(); lineIndex++) {
+
+			var line = descriptionLines.get(lineIndex);
+
+			if (Parser.isSlot(line)) {
+
+				if (slotIndex == slotIndexDestination) {
+					var editedLine = toggleUserMentionInSlot(line, userMention);
+					descriptionLines.set(lineIndex, editedLine);
+				}
+
+				slotIndex++;
+			}
+		}
+
+		return String.join(Parser.SLOT_DELIMITER, descriptionLines);
+	}
+
+	private static String toggleUserMentionInSlot(String input, String userMention) {
+
+		var userMentions = Parser.splitSlotText(input);
+
+		var userAlreadySignedUp = userMentions.contains(userMention);
+		if (userAlreadySignedUp) {
+			userMentions.removeIf(existingMention -> existingMention.equals(userMention));
+		} else {
+			userMentions.add(userMention);
+		}
+
+		return String.join(Parser.SIGNUP_DELIMITER, userMentions);
 	}
 
 }
