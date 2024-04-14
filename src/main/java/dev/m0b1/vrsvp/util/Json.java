e5a284f7fe1e98aff2ae9263d6decb4ecc1dff47
m0b1 <mail@m0b1.dev>
2024-04-14 00:56:51 -0400

    Adjusting object mapper serialization features.

--- src/main/java/dev/m0b1/vrsvp/util/Json.java
+++ src/main/java/dev/m0b1/vrsvp/util/Json.java
@@ -1,6 +1,7 @@
 package dev.m0b1.vrsvp.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,7 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 @UtilityClass
 public final class Json {
 
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+	  .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+	  .enable(SerializationFeature.INDENT_OUTPUT)
+	  .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
 
   public static String write(Object input, String defaultValue) {
 
