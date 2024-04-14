64dbb122946a41aa968bbacec725f8b5e13755d0
m0b1 <mail@m0b1.dev>
2024-04-14 00:56:29 -0400

    Fixing properties class to be properly validated at startup.

--- src/main/java/dev/m0b1/vrsvp/properties/Properties.java
+++ src/main/java/dev/m0b1/vrsvp/properties/Properties.java
@@ -1,18 +1,20 @@
 package dev.m0b1.vrsvp.properties;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties(value = "application.discord")
 @Data
+@Validated
 public class Properties {
 
 	public static final String FORM_NOT_FOUND_REPLY = "Cannot find event post. Was it deleted? Does the bot have read permissions?";
 
-	@NotEmpty
+	@NotBlank
 	private String botSecretToken;
 
 	private String logWebhook;
